2f736486ca5a8a96134525384cdc981eb527cb7e
krinyo <57134381+krinyo@users.noreply.github.com>
2025-04-04 14:40:12 +0500

    Update underroot.sh
    autologin

--- underroot.sh
+++ underroot.sh
@@ -4,3 +4,11 @@ pacman -S xorg-xinit xorg-server i3 awk coreutils i3lock imagemagick util-linux 
 
 #useradd -m -s /bin/bash username
 #passwd username
+
+#sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+#sudo nano /etc/systemd/system/getty@tty1.service.d/override.conf
+
+#[Service]
+#ExecStart=
+#ExecStart=-/sbin/agetty --autologin username --noclear %I $TERM
+#sudo systemctl daemon-reload
